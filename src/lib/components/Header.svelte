c462a533997c2ccce412af0686fa75cf26b7134d
wooffet <wooffet@gmail.com>
2022-08-03 19:42:36 +0100

    feat: :sparkles: add original header html to header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,33 +1,16 @@
-<header>
-	<a href="/">Home</a>
-
-	<nav>
-		<ul>
-			<li><a href="/blog">Blog</a></li>
-			<li><a href="/about">About</a></li>
-			<li><a href="/contact">Contact</a></li>
-		</ul>
-	</nav>
+<header class="text-white bg-[url('/header-bg.jpg')]" style="background-size: cover">
+	<div class="px-8 py-16 bg-gradient-to-r from-black">
+		<div class="max-w-lg grid grid-cols-1 gap-8">
+			<h2 class="text-xl font-bold lowercase">uniquely crafted hand-dyed yarn</h2>
+			<h1 class="text-6xl font-bold">Nightingale Knits</h1>
+			<p class="text-lg lowercase">Est. 2020 | Sam nightingale🐦 | indie yarn dyer</p>
+			<nav>
+				<button
+					class="bg-gradient-to-r from-red-500 to-rose-500 px-6 py-3 text-lg rounded-md lowercase"
+				>
+					Visit my shop
+				</button>
+			</nav>
+		</div>
+	</div>
 </header>
-
-<style>
-	header {
-		padding: 1rem;
-		background: lightskyblue;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-	}
-
-	ul {
-		margin: 0;
-		list-style-type: none;
-		display: flex;
-		gap: 1rem;
-	}
-
-	a {
-		text-decoration: none;
-		color: inherit;
-	}
-</style>
