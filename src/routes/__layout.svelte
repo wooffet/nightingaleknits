4d0dbe6df9c21ce494bbab8eb1e260c2ab6a08b2
wooffet <wooffet@gmail.com>
2022-08-09 19:35:00 +0100

    feat: :sparkles: Add daisyui, remove dark mode stuff

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,16 +2,6 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '$lib/styles/app.css';
-
-	// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-	if (
-		localStorage.theme === 'dark' ||
-		(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-	) {
-		document.documentElement.classList.add('dark');
-	} else {
-		document.documentElement.classList.remove('dark');
-	}
 </script>
 
 <Header />
