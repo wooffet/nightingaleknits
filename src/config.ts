29de67fb6dc7f2535477c5eedc2d630dd11ac2e1
wooffet <wooffet@gmail.com>
2022-08-31 10:54:31 +0100

    feat: :wrench: Update config values

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nightingale Knits';
+export const SITE_DESCRIPTION = 'uniquely crafted hand-dyed yarn | est. 2020 | Sam nightingale🐦 | indie yarn dyer';
